1185b5ef27771f81edd50038c83c55e347ec03b3
Fer Cruz <cruzfernandomartin94@gmail.com>
2024-12-05 23:59:53 -0300

    2 - basic validation of URL done.

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import pandas as pd
 
+def check_url():
+    # inputs from the user
+    application_url = input("Please enter the URL: ")
+    # validates input from the user
+    is_valid_url = ".com" in application_url or ".net" in application_url
+    is_char_valid = not application_url.startswith(".")
+    # evals if the input is a url nor naaaht
+    if is_valid_url and is_char_valid:
+        print("It's a valid URL")
+    else:
+        print("It's not a valid URL. Ending program.")
+
 print("Hola ferchu!")
+
+check_url()
+
+
+
 data = {
     "Job title": ["Software Engineer", "Data Analyst"],
     "Company": ["Company A", "Company B"],
@@ -9,4 +26,4 @@ data = {
 
 df = pd.DataFrame(data)
 
-print(df)
+# print(df)
